1843d441cf4def91e45fefad4cb1945cd934aaaf
Thinh1769 <thinh1769@gmail.com>
2022-05-10 00:03:01 +0700

    authentication Firebase, Create với FireStore

--- app/src/main/java/com/example/dhktpm15a_nhom16/AddScreen.java
+++ app/src/main/java/com/example/dhktpm15a_nhom16/AddScreen.java
@@ -2,6 +2,7 @@ package com.example.dhktpm15a_nhom16;
 
 
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -11,21 +12,27 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class AddScreen extends AppCompatActivity {
 
     EditText txtAddName, txtAddPosition,txtAddAge, txtAddCountry;
-    DataNhanVien dataNhanVien;
-    NhanVien nhanVien;
     String name,position,country,age;
+    FirebaseFirestore db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_screen);
 
-        dataNhanVien = new DataNhanVien(this,"nhanviendb.sqlite",null,1);
+        db = FirebaseFirestore.getInstance();
 
         //nút <- trong main screen
         findViewById(R.id.btnBack1).setOnClickListener(view -> {
@@ -50,23 +57,39 @@ public class AddScreen extends AppCompatActivity {
                 if (TextUtils.isEmpty(name)){
                     txtAddName.setError("Họ tên không được để trống");
                     txtAddName.requestFocus();
-                    }else if (TextUtils.isEmpty(position)){
-                        txtAddPosition.setError("Chức vụ không được để trống");
-                        txtAddPosition.requestFocus();
-                        }else if (TextUtils.isEmpty(age)){
-                            txtAddAge.setError("Tuổi không được để trống");
-                            txtAddAge.requestFocus();
-                            }else if (TextUtils.isEmpty(country)){
-                                 txtAddCountry.setError("Quê quán không được để trống");
-                                 txtAddCountry.requestFocus();
-                                }
+                }else if (TextUtils.isEmpty(position)){
+                    txtAddPosition.setError("Chức vụ không được để trống");
+                    txtAddPosition.requestFocus();
+                }else if (TextUtils.isEmpty(age)){
+                    txtAddAge.setError("Tuổi không được để trống");
+                    txtAddAge.requestFocus();
+                }else if (TextUtils.isEmpty(country)){
+                    txtAddCountry.setError("Quê quán không được để trống");
+                    txtAddCountry.requestFocus();
+                }
                 else {
-                    nhanVien = new NhanVien(name, position, Integer.parseInt(age), country);
+                    Map<String, Object> nhanVien = new HashMap<>();
+                    nhanVien.put("name", name);
+                    nhanVien.put("position", position);
+                    nhanVien.put("age", age);
+                    nhanVien.put("country", country);
 
-                    dataNhanVien.addNhanVien(nhanVien);
-                    Toast.makeText(AddScreen.this, "Thêm nhân viên thành công", Toast.LENGTH_SHORT).show();
-                    Intent intent = new Intent(AddScreen.this, MainScreen.class);
-                    startActivity(intent);
+                    db.collection("NhanVien")
+                            .add(nhanVien)
+                            .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                                @Override
+                                public void onSuccess(DocumentReference documentReference) {
+                                    Toast.makeText(AddScreen.this, "Thêm nhân viên thành công", Toast.LENGTH_SHORT).show();
+                                    Intent intent = new Intent(AddScreen.this, MainScreen.class);
+                                    startActivity(intent);
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Toast.makeText(AddScreen.this, "Thêm nhân viên thất bại! Lỗi : "+e.getMessage(), Toast.LENGTH_SHORT).show();
+                                }
+                            });
                 }
             }
         });
